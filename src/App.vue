962ed5a5e01246617e248911c7a6f4e2b23e124a
Иван Синяков <sia@ipap.local>
2025-05-09 03:20:21 +1000

    initial

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+const isAppLoaded = ref(false);
+
+onMounted(() => {
+  Telegram.WebApp.ready();
+  Telegram.WebApp.requestFullscreen();
+
+  isAppLoaded.value = true;
+});
+</script>
+
+<template>
+  <div class="w-full h-full overflow-hidden">
+    <video
+      v-if="isAppLoaded"
+      src="@/assets/video.mp4"
+      muted
+      autoplay
+      class="h-full w-full object-cover"></video>
+  </div>
+</template>
+
+<style scoped></style>
